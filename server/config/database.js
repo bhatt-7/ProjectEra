b035748753b30a171f0f4428d03aeef14afd243a
Ayush Bhatt <ayushbhatt11001100@gmail.com>
2024-06-20 12:57:49 +0530

    commiting

--- server/config/database.js
+++ server/config/database.js
@@ -4,10 +4,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
